7c5def4bb2588e326ffb9e800536c669310212bb
Jing-Shan Wong <joshdragonwong@gmail.com>
2019-10-08 09:55:14 -0600

    completed bigger_two

--- list_quiz.rb
+++ list_quiz.rb
@@ -14,4 +14,27 @@ end
 #print three_even([1, 2, 3, 4, 5, 6]) #True
 #print three_even([1, 2, 3, 4, 5, 5]) #False
 
+def bigger_two(list, list1)
+    total = 0
+    total1 = 0
+    list.each do |x|
+        total = x + total
+    end
+    list1.each do |x|
+        total1 = x + total1
+    end
+    # printing the list
+    if total > total1
+        return list
+    end
+    if total < total1
+        return list1
+    end
+    if total == total1
+        return list
+    end
+end
 
+#print bigger_two([1, 2], [2, 3])
+#print bigger_two([1,2], [1,2])
+#print bigger_two([7,8], [1,2])
